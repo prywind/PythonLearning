a9b537ad167d8958ac1dcbfee454fa1afe5a1853
Ilya <ilya11997@gmail.com>
2023-08-22 17:49:13 +0300

    Привет новая оболочка!

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+# Модули
+from colorama import init
+from colorama import Fore, Back, Style
+
+# Переменные
+print(Back.GREEN)
+print(Fore.BLACK)
+a = input("Первое число:\n")
+op = input("Что делаем?(+,-,/,*)\n")
+b = input("Второе число:\n")
+
+
+# Проверка знака
+if op == "+":
+    c = float(a) + float(b)
+elif op == "-":
+    c = float(a) - float(b)
+elif op == "/":
+    c = float(a) / float(b)
+elif op == "*":
+    c = float(a) * float(b)
+else:
+    c = 0
+print(Back.BLACK)
+print(Fore.WHITE)
+print("Ответ: " + str(c))
+
+input()
